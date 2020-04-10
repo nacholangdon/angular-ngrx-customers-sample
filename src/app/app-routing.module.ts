5791da1ea91b20390340f78c956439e41dd8bd56
Juan Ignacio Langdon Sagasta <nacholangdon@gmail.com>
2020-04-09 21:27:52 -0300

    Add ngrx dependencies and initial components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'customers',
+    loadChildren: '../app/customers/customers.module#CustomersModule'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
