038b892f01bea52026284cae47e9b82c37ae7f5b
Juan Ignacio Langdon Sagasta <nacholangdon@gmail.com>
2020-04-09 21:28:19 -0300

    Add ngrx dependencies and initial components

---
+++ src/app/customers/customers.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerAddComponent } from './customer-add/customer-add.component';
+import { CustomerEditComponent } from './customer-edit/customer-edit.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+
+
+
+@NgModule({
+  declarations: [CustomerComponent, CustomerAddComponent, CustomerEditComponent, CustomerListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CustomersModule { }
